614ddf59dd49a29e8874e2ca737c6e61764e81cc
Dana Palmer <roiai4@hotmail.com>
2018-07-14 09:33:07 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -5,10 +5,11 @@ def my_each(nukacola)
     yield(nukacola[caps])
     caps = caps + 1 
   end
-nukacola
+my_each(nukacola)
   if nukacola.empty?
     "You can't pay for this!"
-  else
-    "That's all?"
+  else nukacola.map do |afford|
+    afford = []
+    afford << nukacola
 end
 end
