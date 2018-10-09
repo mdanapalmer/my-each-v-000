492d765388d3f78ac1be9850c4a11a1b17a0257b
Dana Palmer <roiai4@hotmail.com>
2018-10-09 01:00:42 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,7 @@
-def my_each(nukacola)
-    caps = 0 
+def my_each
+  counter = 0
+
+  while counter > 0
     
-    while caps < 4
-    yield(nukacola[caps])
-    caps = caps + 1 
-  end
-my_each(nukacola)
-  if nukacola.empty?
-    "You can't pay for this!"
-  else nukacola.map do |afford|
-    afford = []
-    afford << nukacola
-end
+
 end
